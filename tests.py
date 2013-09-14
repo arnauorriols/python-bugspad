53617ea2c05fe5c0a5c633ff21e72f4aaf4fb931
Arnau Orriols <josepcaselles@gmail.com>
2013-09-14 18:57:39 +0200

    Adds new testcase for get_components

--- tests.py
+++ tests.py
@@ -114,6 +114,15 @@ class BugTest(unittest.TestCase):
         for component in response.iteritems():
             self.assertEqual(component[0], component[1][1])
 
+    @unittest.skip("Too slow, comment this line to test this function")
+    def test_get_components_list_auth_not_required(self):
+        response = self.wrong_auth_bug.get_components_list(2)
+
+        self.assertIs(type(response), dict)
+        self.assertNotEqual(len(response), 0)
+        for component in response.iteritems():
+            self.assertEqual(component[0], component[1][1])
+
 
     def test_add_component_returns_component_info_dict(self):
         response = self.with_id_bug.add_component('new_component',
